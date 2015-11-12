02eef06640e0868b43747550b5dcb1aa1ed1d353
jangsoopark <wkdtn8211@gmail.com>
2015-11-13 04:20:28 +0900

    |

--- firewall/expired.c
+++ firewall/expired.c
@@ -146,6 +146,7 @@ int SendData(klp_socket_t sock_fd, klp_flow *data)
 	len = sprintf(buf, "%u|%u|%u|%u|%u|%u|%u|%u|%u|%s|%s|%d",
 		data->key.saddr, data->key.src, data->key.daddr, data->key.dst, data->key.tcpudp,
 		data->warn, data->danger, data->packet_count, data->totalbytes, data->starttime, data->endtime, 0);
+
 	data_count = klp_write(sock_fd, buf, len, 0);
 	//printk("%s %d\n", buf, data_count);
 
